84648f347af444d91e50885bf7ff3fc6ba8fd639
wooktori <jjun9503@gmail.com>
2025-03-02 03:09:41 +0900

    Coin 페이지 만들기

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,20 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 import Coins from "./components/Coins";
 import Coin from "./components/Coin";
 import App from "./App";
+import Chart from "./components/Chart";
+import Price from "./components/Price";
 
 export const router = createBrowserRouter([
-    {
-        path: "",
-        element: <App />,
+  {
+    path: "",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Coins />,
+      },
+      {
+        path: ":coinId",
+        element: <Coin />,
         children: [
-            {
-                path: "/",
-                element: <Coins />,
-            },
-            {
-                path: ":coinId",
-                element: <Coin />,
-            },
+          { path: "price", element: <Price /> },
+          { path: "chart", element: <Chart /> },
         ],
-    },
+      },
+    ],
+  },
 ]);
--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -3,92 +3,90 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
-    max-width: 480px;
-    margin: 0 auto;
-    padding: 0 20px;
+  max-width: 480px;
+  margin: 0 auto;
+  padding: 0 20px;
 `;
 const Header = styled.header`
-    height: 15vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 const CoinsList = styled.ul``;
 const Coin = styled.li`
-    background-color: white;
-    color: ${(props) => props.theme.bgColor};
-    border-radius: 15px;
-    margin-bottom: 10px;
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    display: flex;
+    align-items: center;
+  }
+  &:hover {
     a {
-        padding: 20px;
-        transition: color 0.2s ease-in;
-        display: flex;
-        align-items: center;
+      color: ${(props) => props.theme.accentColor};
     }
-    &:hover {
-        a {
-            color: ${(props) => props.theme.accentColor};
-        }
-    }
+  }
 `;
 const Title = styled.h1`
-    color: ${(props) => props.theme.accentColor};
+  color: ${(props) => props.theme.accentColor};
 `;
 const Loader = styled.span`
-    text-align: center;
-    display: block;
+  text-align: center;
+  display: block;
 `;
 const Img = styled.img`
-    width: 35px;
-    height: 35px;
-    margin-right: 10px;
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
-    id: string;
-    name: string;
-    symbol: string;
-    rank: number;
-    is_new: boolean;
-    is_active: boolean;
-    type: string;
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
 }
 
 export default function Coins() {
-    const [coins, setCoins] = useState<CoinInterface[]>([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        (async () => {
-            const response = await fetch(
-                "https://api.coinpaprika.com/v1/coins"
-            );
-            const json = await response.json();
-            setCoins(json.slice(0, 100));
-        })();
-        setLoading(false);
-    }, []);
-    return (
-        <Container>
-            <Header>
-                <Title>코인</Title>
-            </Header>
-            {loading ? (
-                <Loader>Loading...</Loader>
-            ) : (
-                <CoinsList>
-                    {coins.map((coin) => (
-                        <Coin key={coin.id}>
-                            <Link to={coin.id} state={{ name: coin.name }}>
-                                {" "}
-                                <Img
-                                    src={`https://static.coinpaprika.com/coin/${coin.id}/logo.png`}
-                                />
-                                {coin.name} &rarr;
-                            </Link>
-                        </Coin>
-                    ))}
-                </CoinsList>
-            )}
-        </Container>
-    );
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+    })();
+    setLoading(false);
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={coin.id} state={{ name: coin.name }}>
+                {" "}
+                <Img
+                  src={`https://static.coinpaprika.com/coin/${coin.id}/logo.png`}
+                />
+                {coin.name} &rarr;
+              </Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
 }
